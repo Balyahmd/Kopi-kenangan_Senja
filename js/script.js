7f2d3de2da4801da0fb38cafc89e33575b590b51
balyaahmad133@gmail.com <balyaahmad133@gmail.com>
2023-02-24 04:14:57 +0700

    membuat website kedai kopi

---
+++ js/script.js
@@ -0,0 +1,16 @@
+//Toggle class active
+const navbarNav = document.querySelector(".navbar-nav");
+
+//Ketika Hamburger menu diklik
+document.querySelector("#hamburger-menu").onclick = () => {
+  navbarNav.classList.toggle("active");
+};
+
+// klik diluar sidebar untuk menghilangkan nav
+const hamburger = document.querySelector("#hamburger-menu");
+
+document.addEventListener("click", function (e) {
+  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+});
